cf811430675671e66e57416c1690b81ff7c0b7fa
Amir <aeonthings@gmail.com>
2025-02-03 17:15:27 +0100

    a

---
+++ data_ingestion/exchanges/__init__.py
@@ -0,0 +1,2 @@
+# exchanges/__init__.py
+# This package handles data ingestion from multiple exchanges and on-chain sources.
---
+++ data_ingestion/onchain/__init__.py
@@ -0,0 +1,2 @@
+# onchain/__init__.py
+# This package handles data ingestion from multiple exchanges and on-chain sources.
